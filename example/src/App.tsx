a22a945823e655648945f59a3fd15f98054bf2ce
winrid <winrid@gmail.com>
2025-11-19 19:41:17 -0800

    Adding page url and title to the examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,10 +7,14 @@ import type { FastCommentsCommentWidgetConfig } from 'fastcomments-typescript';
 export default function App() {
   const myTenantId = 'demo'; // Your tenant id. Can be fetched from https://fastcomments.com/auth/my-account/api-secret
   const myAppPageId = 'native-test'; // the ID or URL of the comment thread in your app.
+  const myAppPageUrl = 'https://example.com/external-page'; // you can optional set a url to an external page
+  const myAppPageTitle = 'Example Title'; // ... and you probably want a title for this content
 
   const [config] = useState<FastCommentsCommentWidgetConfig>({
     tenantId: myTenantId,
     urlId: myAppPageId,
+    url: myAppPageUrl,
+    pageTitle: myAppPageTitle
   });
 
   // Uncomment this to test changing pages without reloading the whole widget.
